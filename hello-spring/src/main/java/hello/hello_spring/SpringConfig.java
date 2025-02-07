95ac47439ce08e6eb0f07ebdaf17ab8387333db0
dona0123 <ehsk0123@naver.com>
2025-02-07 21:05:04 +0900

    [week05] 스프링 섹션 7 : 스프링 DB 접근 기술 - 순수 JDBC

--- hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,13 +1,22 @@
 package hello.hello_spring;
 
+import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     // MemberService 생성 후 스프링 빈에 등록
     @Bean
@@ -18,6 +27,7 @@ public class SpringConfig {
     // MemberRepository 생성 후 스프링 빈에 등록
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
