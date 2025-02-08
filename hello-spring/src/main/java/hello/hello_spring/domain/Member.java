baf16286629b9d8ac94bf3343668572ef22d56a5
dona0123 <ehsk0123@naver.com>
2025-02-08 18:52:45 +0900

    [week05] 스프링 섹션 7 : 스프링 DB 접근 기술 - JPA

--- hello-spring/src/main/java/hello/hello_spring/domain/Member.java
+++ hello-spring/src/main/java/hello/hello_spring/domain/Member.java
@@ -1,7 +1,17 @@
 package hello.hello_spring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // jpa가 관리하는 엔티티
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // id 자동생성
     private Long id;
+
     private String name;
 
     public Long getId() {
